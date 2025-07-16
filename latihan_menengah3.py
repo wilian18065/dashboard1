60c5f94f6a9112025c3b28bb7441c55fe0ca6de4
wilian18065 <82296856+wilian18065@users.noreply.github.com>
2025-07-16 07:10:49 +0700

    Update latihan_menengah3.py

--- latihan_menengah3.py
+++ latihan_menengah3.py
@@ -1,8 +1,5 @@
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker   
+import pandas as pd 
 import plotly.express as px
 import seaborn as sns
 
